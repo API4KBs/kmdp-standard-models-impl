10feb439efb4de32cfb12aa6aeb34d79e9d94e15
Davide Sottara <Sottara.Davide@mayo.edu>
2020-11-12 01:38:26 -0600

    [#329909] Remove legacy Surrogate v1 code

--- kmdp-asset-metadata/src/main/java/edu/mayo/kmdp/terms/ControlledTerm.java
+++
@@ -1,9 +0,0 @@
-package edu.mayo.kmdp.terms;
-
-/**
- * Marker interface for terms that are defined in a controlled vocabulary,
- * possibly derived from an ontology or concept scheme
- */
-public interface ControlledTerm {
-
-}
